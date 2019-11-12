85aa10b5e634a65c83be2c3a552b57b29580c5b8
Lance Miller <lance.miller@genesys.com>
2019-11-12 10:40:31 -0500

    added the functionality from other branch without deletes

--- hackathon-2019/contact-api/DB.py
+++ hackathon-2019/contact-api/DB.py
@@ -1,75 +1,73 @@
 import uuid
 import psycopg2
 
+
 class ContactDB:
     ##Need to put this in a secret
     def __init__(self, _user, _password, _dbname, _host):
-         self.user = _user
-         self.password = _password
-         self.dbname = _dbname
-         self.host=_host
-
+        self.user = _user
+        self.password = _password
+        self.dbname = _dbname
+        self.host = _host
 
     def getConn(self):
-        return  psycopg2.connect(user=self.user, password=self.password,dbname=self.dbname, host=self.host)
+        return psycopg2.connect(user=self.user, password=self.password, dbname=self.dbname, host=self.host)
 
-         
     def createContactTable(self):
         conn = self.getConn()
         cursor = conn.cursor()
         cursor.execute("DROP TABLE IF EXISTS contacts;")
         cursor.execute("CREATE TABLE contacts (contact_id VARCHAR(100) PRIMARY KEY NOT NULL, " +
-                 "first_name TEXT NOT NULL, "     +
-                 "last_name  TEXT NOT NULL, "     +
-                 "phone      TEXT NOT NULL, "     +
-                 "group_id   TEXT NOT NULL  ); ")
-          
+                       "first_name TEXT NOT NULL, " +
+                       "last_name  TEXT NOT NULL, " +
+                       "phone      TEXT NOT NULL, " +
+                       "group_id   TEXT NOT NULL  ); ")
+
         conn.commit()
         conn.close()
 
-
-    def create(self, first_name, last_name, phone,groupId):
+    def create(self, first_name, last_name, phone, groupId):
         contactId = str(uuid.uuid4())
 
         conn = self.getConn()
         cursor = conn.cursor()
         cursor.execute(
             "INSERT INTO contacts (contact_id, first_name, last_name, phone, group_id) VALUES ((%s),(%s),(%s),(%s),(%s))",
-            (contactId,first_name,last_name,phone,groupId))
+            (contactId, first_name, last_name, phone, groupId))
         conn.commit()
         conn.close()
         return contactId
 
-    def get(self,contactId):
+    def get(self, contactId):
         conn = self.getConn()
         cursor = conn.cursor()
-         
-        cursor.execute("SELECT * FROM contacts WHERE contact_id = %(contactId)s",    {'contactId': contactId})
+
+        cursor.execute("SELECT * FROM contacts WHERE contact_id = %(contactId)s", {'contactId': contactId})
         dbresult = cursor.fetchone()
         result = {}
         result["contactId"] = contactId
         result["firstName"] = dbresult[1]
-        result["lastName"]  = dbresult[2]
-        result["phoneNumber"]     = dbresult[3]
+        result["lastName"] = dbresult[2]
+        result["phoneNumber"] = dbresult[3]
         conn.close()
-        return result 
+        return result
 
-    def getAllByGroupId(self,groupId):
+    def getAllByGroupId(self, groupId):
         conn = self.getConn()
-        cursor = conn.cursor() 
-        cursor.execute("SELECT * FROM contacts WHERE group_id = %(groupId)s",    {'groupId': groupId})
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM contacts WHERE group_id = %(groupId)s", {'groupId': groupId})
         results = []
 
         for record in cursor:
-          result = {}
-          result["contactId"] = record[0]
-          result["firstName"] = record[1]
-          result["lastName"]  = record[2]
-          result["phoneNumber"]     = record[3]
-          result["groupId"]   = record[4]
-          results.append(result)
-        conn.close() 
-        return results 
+            result = {}
+            result["contactId"] = record[0]
+            result["firstName"] = record[1]
+            result["lastName"] = record[2]
+            result["phoneNumber"] = record[3]
+            result["groupId"] = record[4]
+            results.append(result)
+        conn.close()
+        return results
 
     def getAll(self):
         conn = self.getConn()
@@ -78,15 +76,15 @@ class ContactDB:
         cursor.execute("SELECT * FROM contacts")
 
         for record in cursor:
-          result = {}
-          result["contactId"] = record[0]
-          result["firstName"] = record[1]
-          result["lastName"]  = record[2]
-          result["phoneNumber"]     = record[3]
-          result["groupId"]   = record[4]
-          results.append(result)
-        conn.close()  
-        return results      
+            result = {}
+            result["contactId"] = record[0]
+            result["firstName"] = record[1]
+            result["lastName"] = record[2]
+            result["phoneNumber"] = record[3]
+            result["groupId"] = record[4]
+            results.append(result)
+        conn.close()
+        return results
 
     def delete(self):
         print("Delete Contact.")
@@ -94,25 +92,24 @@ class ContactDB:
 
 class GroupDB:
     def __init__(self, _user, _password, _dbname, _host):
-         self.user = _user
-         self.password = _password
-         self.dbname = _dbname
-         self.host=_host
+        self.user = _user
+        self.password = _password
+        self.dbname = _dbname
+        self.host = _host
 
     def getConn(self):
-        return  psycopg2.connect(user=self.user, password=self.password,dbname=self.dbname, host=self.host)
-         
+        return psycopg2.connect(user=self.user, password=self.password, dbname=self.dbname, host=self.host)
+
     def createGroupTable(self):
         conn = self.getConn()
         cursor = conn.cursor()
         cursor.execute("DROP TABLE IF EXISTS groups;")
         cursor.execute("CREATE TABLE groups (group_id VARCHAR(100) PRIMARY KEY NOT NULL, " +
-                 "group_name         TEXT NOT NULL, "     +
-                 "group_description  TEXT NOT NULL); ")
+                       "group_name         TEXT NOT NULL, " +
+                       "group_description  TEXT NOT NULL); ")
 
         conn.commit()
-        conn.close()         
-
+        conn.close()
 
     def create(self, group_name, group_description):
         groupId = str(uuid.uuid4())
@@ -120,25 +117,25 @@ class GroupDB:
         cursor = conn.cursor()
         cursor.execute(
             "INSERT INTO groups (group_id, group_name, group_description) VALUES ((%s),(%s),(%s))",
-            (groupId,group_name,group_description))
+            (groupId, group_name, group_description))
         conn.commit()
 
         conn.close()
         return groupId
 
-    def get(self,groupId):
+    def get(self, groupId):
         conn = self.getConn()
         cursor = conn.cursor()
-         
-        cursor.execute("SELECT * FROM groups WHERE group_id = %(groupId)s",    {'groupId': groupId})
+
+        cursor.execute("SELECT * FROM groups WHERE group_id = %(groupId)s", {'groupId': groupId})
         dbresult = cursor.fetchone()
         result = {}
-        result["groupId"]   = groupId
+        result["groupId"] = groupId
         result["groupName"] = dbresult[1]
-        result["groupDescription"]  = dbresult[2]
-        result["groupId"] =dbresult[3]
+        result["groupDescription"] = dbresult[2]
+        result["groupId"] = dbresult[3]
         conn.close()
-        return result 
+        return result
 
     def getAll(self):
         conn = self.getConn()
@@ -147,13 +144,119 @@ class GroupDB:
         cursor.execute("SELECT * FROM groups")
 
         for record in cursor:
-              result = {}
-              result["groupId"] = record[0]
-              result["groupName"] = record[1]
-              result["groupDescription"] = record[2]
-              results.append(result)
+            result = {}
+            result["groupId"] = record[0]
+            result["groupName"] = record[1]
+            result["groupDescription"] = record[2]
+            results.append(result)
         conn.close()
-        return results                   
+        return results
 
     def delete(self):
         print("Delete Group.")
+
+
+class RfidDB:
+    def __init__(self, _user, _password, _dbname, _host):
+        self.user = _user
+        self.password = _password
+        self.dbname = _dbname
+        self.host = _host
+
+    def getConn(self):
+        return psycopg2.connect(user=self.user, password=self.password, dbname=self.dbname, host=self.host)
+
+    def createRfidUsersTable(self):
+        conn = self.getConn()
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS RfidUsers;")
+        cursor.execute("CREATE TABLE RfidUsers (rfid VARCHAR(100) PRIMARY KEY NOT NULL, " +
+                       "group_id  VARCHAR(100) NOT NULL, " +
+                       "musicOption_id VARCHAR(100) NOT NULL); ")
+
+        conn.commit()
+        conn.close()
+
+    def createRfidUser(self, rfid, group_id, musicOption_id):
+        conn = self.getConn()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO RfidUsers (rfid, group_id, musicOption_id) VALUES ((%s),(%s),(%s))",
+            (rfid, group_id, musicOption_id))
+        conn.commit()
+        conn.close()
+        return rfid
+
+    def getByRfid(self, rfid):
+        conn = self.getConn()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM RfidUsers WHERE rfid = %(rfID)s", {'rfID': rfid})
+        dbresult = cursor.fetchone()
+        result = {}
+        result["rfid"] = dbresult[0]
+        result["group_id"] = dbresult[1]
+        result["musicOption_id"] = dbresult[2]
+        conn.close()
+        return result
+
+
+class MusicOptionDB:
+    def __init__(self, _user, _password, _dbname, _host):
+        self.user = _user
+        self.password = _password
+        self.dbname = _dbname
+        self.host = _host
+
+    def getConn(self):
+        return psycopg2.connect(user=self.user, password=self.password, dbname=self.dbname, host=self.host)
+
+    def createMusicOptionTable(self):
+        conn = self.getConn()
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS MusicOptions;")
+        cursor.execute("CREATE TABLE MusicOptions (musicOption_id VARCHAR(100) NOT NULL PRIMARY KEY, " +
+                       "musicTitle TEXT NOT NULL, " +
+                       "fileName  TEXT NOT NULL); ")
+
+        conn.commit()
+        conn.close()
+
+    def createMusicOptionEntry(self, musicTitle, fileName):
+        conn = self.getConn()
+        musicID = str(uuid.uuid4())
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO MusicOptions (musicOption_id, musicTitle, fileName) VALUES ((%s),(%s),(%s))",
+            (musicID, musicTitle, fileName))
+        conn.commit()
+        conn.close()
+        return musicID
+
+    def getAllMusicOptions(self):
+        conn = self.getConn()
+        cursor = conn.cursor()
+        results = []
+        cursor.execute("SELECT * FROM MusicOptions")
+
+        for record in cursor:
+            result = {}
+            result["musicOption_id"] = record[0]
+            result["musicTitle"] = record[1]
+            results.append(result)
+        conn.close()
+        return results
+
+    def getByMusicId(self, musicOption_id):
+        conn = self.getConn()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM MusicOptions WHERE musicOption_id = %(musicOptionID)s",
+                       {'musicOptionID': musicOption_id})
+        dbresult = cursor.fetchone()
+        result = {}
+        result["musicOption_id"] = dbresult[0]
+        result["musicTitle"] = dbresult[1]
+        result["fileName"] = dbresult[2]
+        conn.close()
+        return result
--- hackathon-2019/contact-api/contact.py
+++ hackathon-2019/contact-api/contact.py
@@ -1,6 +1,6 @@
-#/usr/local/bin/python3
+# /usr/local/bin/python3
 from flask import Flask, jsonify, request
-from DB import ContactDB,GroupDB
+from DB import ContactDB, GroupDB, RfidDB, MusicOptionDB
 import json
 import time
 from httplib2 import Http
@@ -13,100 +13,162 @@ import time
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-#@app.before_first_request
+
+# @app.before_first_request
 def init_app():
     logger.info("Initializing the contact service.  ")
     global contactDB
     global groupDB
+    global rfidDB
+    global musicOptionDB
     logger.info("User: {}".format(os.environ["POSTGRES_USER"]))
-    logger.info("DB: {}".format( os.environ["POSTGRES_DB"]))
-    logger.info("HOST: {}".format( os.environ["POSTGRES_HOSTNAME"]))
+    logger.info("DB: {}".format(os.environ["POSTGRES_DB"]))
+    logger.info("HOST: {}".format(os.environ["POSTGRES_HOSTNAME"]))
     logger.info("Creating the contact db connection")
     contactDB = ContactDB(os.environ["POSTGRES_USER"],
-                             os.environ["POSTGRES_PASSWORD"],
-                             os.environ["POSTGRES_DB"],
-                             os.environ["POSTGRES_HOSTNAME"]  )
-    logger.info("Creating the groupdb connection")            
-    groupDB = GroupDB(   os.environ["POSTGRES_USER"],
-                             os.environ["POSTGRES_PASSWORD"],
-                              os.environ["POSTGRES_DB"],
-                             os.environ["POSTGRES_HOSTNAME"]  )  
-    logger.info("Creating the contact db table")                      
+                          os.environ["POSTGRES_PASSWORD"],
+                          os.environ["POSTGRES_DB"],
+                          os.environ["POSTGRES_HOSTNAME"])
+    logger.info("Creating the groupdb connection")
+    groupDB = GroupDB(os.environ["POSTGRES_USER"],
+                      os.environ["POSTGRES_PASSWORD"],
+                      os.environ["POSTGRES_DB"],
+                      os.environ["POSTGRES_HOSTNAME"])
+    logger.info("Creating the rfidDB connection")
+    rfidDB = RfidDB(os.environ["POSTGRES_USER"],
+                    os.environ["POSTGRES_PASSWORD"],
+                    os.environ["POSTGRES_DB"],
+                    os.environ["POSTGRES_HOSTNAME"])
+    logger.info("Creating the musicoOtiondb connection")
+    musicOptionDB = MusicOptionDB(os.environ["POSTGRES_USER"],
+                                  os.environ["POSTGRES_PASSWORD"],
+                                  os.environ["POSTGRES_DB"],
+                                  os.environ["POSTGRES_HOSTNAME"])
+    logger.info("Creating the contact db table")
     contactDB.createContactTable()
-    logger.info("Creating the Group db table")         
-    groupDB.createGroupTable()  
+    logger.info("Creating the Group db table")
+    groupDB.createGroupTable()
+    logger.info("Creating the RFID db table")
+    rfidDB.createRfidUsersTable()
+    logger.info("Creating the Music Options db table")
+    musicOptionDB.createMusicOptionTable()
+
 
 @app.after_request
 def per_request_callbacks(response):
-    response.headers['tmx-correlation-id']=str(utils.request_id())
-    return response                            
+    response.headers['tmx-correlation-id'] = str(utils.request_id())
+    return response
+
 
 @app.route('/health/check')
 def healthCheck():
-    return jsonify({'status': 'OK'}),200
+    return jsonify({'status': 'OK'}), 200
+
 
 @app.route('/api/v1.0/contacts', methods=['POST'])
 def create_contact():
-    logger.debug("Creating a contact record:  FirstName: {}. LastName: {}. Phone {}. Group Id {}".format(request.json["firstName"],request.json["lastName"],request.json["phone"], request.json["groupId"]))
+    logger.debug("Creating a contact record:  FirstName: {}. LastName: {}. Phone {}. Group Id {}".format(
+        request.json["firstName"], request.json["lastName"], request.json["phone"], request.json["groupId"]))
 
-    contactId  = contactDB.create( request.json["firstName"],request.json["lastName"],request.json["phone"], request.json["groupId"]) 
-    return  jsonify({'contactId': contactId}), 201
+    contactId = contactDB.create(request.json["firstName"], request.json["lastName"], request.json["phone"],
+                                 request.json["groupId"])
+    return jsonify({'contactId': contactId}), 201
+
+
+@app.route('/api/v1.0/rfid', methods=['POST'])
+def create_rfidUser():
+    logger.debug("Creating a rfid record:  RFID code: {}. GroupID: {}. MusicOption {}".format(request.json["rfId"],
+                                                                                              request.json["group_id"],
+                                                                                              request.json[
+                                                                                                  "musicOption_id"]))
+
+    rfid = rfidDB.createRfidUser(request.json["rfId"], request.json["group_id"], request.json["musicOption_id"])
+    return jsonify({'rfid': rfid}), 201
+
 
 @app.route('/api/v1.0/contacts/<contactId>', methods=['GET'])
 def get_contact(contactId):
     logger.debug("Retrieve contact {} from database.".format(contactId))
-    contact  = contactDB.get( contactId) 
-    return  jsonify(contact), 200
+    contact = contactDB.get(contactId)
+    return jsonify(contact), 200
+
+
+@app.route('/api/v1.0/rfid/<rfidCode>', methods=['GET'])
+def get_rfidEntry(rfidCode):
+    logger.debug("Retrieve rfid Entry {} from database.".format(rfidCode))
+    rfidEntry = rfidDB.getByRfid(rfidCode)
+    return jsonify(rfidEntry), 200
+
+
+@app.route('/api/v1.0/musicOptions/<rfidCode>', methods=['GET'])
+def get_musicOptionByRfid(rfidCode):
+    logger.debug("Retrieve rfid Entry {} from database.".format(rfidCode))
+    musicOptionID = (rfidDB.getByRfid(rfidCode))["musicOption_id"]
+    musicOption = musicOptionDB.getByMusicId(musicOptionID)
+    return jsonify(musicOption), 200
+
 
 @app.route("/api/v1.0/contacts", methods=['GET'])
 def get_all_contacts():
-   logger.debug("Retrieving al contacts in the database") 
-   contacts =  contactDB.getAll()
-   return jsonify(contacts), 200    
+    logger.debug("Retrieving al contacts in the database")
+    contacts = contactDB.getAll()
+    return jsonify(contacts), 200
+
 
 @app.route("/api/v1.0/groups", methods=['GET'])
 def get_all_groups():
-   logger.info("Retrieving all groups from the /api/v1.0/groups method")
-   groups =  groupDB.getAll()
-   return jsonify(groups), 200    
+    logger.info("Retrieving all groups from the /api/v1.0/groups method")
+    groups = groupDB.getAll()
+    return jsonify(groups), 200
+
+
+@app.route("/api/v1.0/musicOptions", methods=['GET'])
+def get_all_musicOptions():
+    logger.info("Retrieving all music options from the /api/v1.0/musicOptions method")
+    musicOptions = musicOptionDB.getAllMusicOptions()
+    return jsonify(musicOptions), 200
+
 
 @app.route('/api/v1.0/contacts/groups/<groupId>', methods=['GET'])
 def get_contact_by_group(groupId):
     logger.debug("Retrieving all contacts belong to group {}.".format(groupId))
-    contacts  = contactDB.getAllByGroupId( groupId ) 
-    return  jsonify(contacts), 200  
+    contacts = contactDB.getAllByGroupId(groupId)
+    return jsonify(contacts), 200
+
 
 @app.route('/api/v1.0/contacts/groups/<groupId>/notify', methods=['POST'])
 def post_contact_by_group(groupId):
     text = request.json["msgText"]
-    for contact in contactDB.getAllByGroupId( groupId ): 
-         logger.info("Attempting to notify contact: {} {} with message {}".format(contact["firstName"],
-                                                                                   contact["lastName"],
-                                                                                   text ))
-         contact["msgText"] = text
-         contactBody = json.dumps({"contacts": [contact]})
-         
-        
-         http_obj = Http(".cache")
-         (resp, content) = http_obj.request(uri="http://notifier:5010/api/v1.0/notify"
-              ,method='POST',body=contactBody, headers={'Content-type': 'application/json',
-                                                        'tmx-correlation-id': str(utils.request_id())})
+    for contact in contactDB.getAllByGroupId(groupId):
+        logger.info("Attempting to notify contact: {} {} with message {}".format(contact["firstName"],
+                                                                                 contact["lastName"],
+                                                                                 text))
+        contact["msgText"] = text
+        contactBody = json.dumps({"contacts": [contact]})
 
-         time.sleep(.5)                                                
+        http_obj = Http(".cache")
+        (resp, content) = http_obj.request(uri="http://notifier:5010/api/v1.0/notify"
+                                           , method='POST', body=contactBody,
+                                           headers={'Content-type': 'application/json',
+                                                    'tmx-correlation-id': str(utils.request_id())})
 
-         if (resp.status==200):
-           app.logger.info("Retrieved content back from: {}".format(content.decode("utf-8")))
-         else:
-           print("Error encountered when looking up contact data.  No data returned")
-    
-    return  jsonify([]), 200  
+        time.sleep(.5)
+
+        if (resp.status == 200):
+            app.logger.info("Retrieved content back from: {}".format(content.decode("utf-8")))
+        else:
+            print("Error encountered when looking up contact data.  No data returned")
+
+    return jsonify([]), 200
+
 
 @app.route('/api/v1.0/contacts/seed', methods=['POST'])
 def post_load_seed_data():
     logger.info("Loading the seed data into the database")
     loadSeedData()
-    
-    return  jsonify([]), 200  
+
+    return jsonify([]), 200
+
 
 def loadSeedData():
     init_app()
@@ -118,7 +180,7 @@ def loadSeedData():
     logger.info("Loading the second piece of group data.")
     groupId2 = groupDB.create("Schneider Trucking", "Schneider Trucking Executive Leadership Group")
     contactId = contactDB.create("Mike", "Mckhehan", "19202651555", groupId2)
-    contactId = contactDB.create("Dan", "Goerdt", "19202652322", groupId2) 
+    contactId = contactDB.create("Dan", "Goerdt", "19202652322", groupId2)
 
     logger.info("Loading the third group of data.")
     groupId3 = groupDB.create("Schneider Trucking", "Edge Infrastructure Room")
@@ -127,6 +189,14 @@ def loadSeedData():
     contactId = contactDB.create("Sean", "Xiao", "19193608657", groupId3)
     contactId = contactDB.create("Brad", "Segiobiano", "19194279072", groupId3)
 
+    logger.info("Loading the fourth group of data.")
+    musicOption1 = musicOptionDB.createMusicOptionEntry("Whatta Man - Salt and Pepper", "WhattaMan.mp3")
+    musicOption2 = musicOptionDB.createMusicOptionEntry("Crazy Train - Ozzy Osbourne", "CrazyTrain.mp3")
+    musicOption3 = musicOptionDB.createMusicOptionEntry("Imperial March - Star Wars", "ImperialMarch.mp3")
+    rfid = rfidDB.createRfidUser("11111111", groupId, musicOption1)
+    rfid = rfidDB.createRfidUser("22222222", groupId2, musicOption2)
+    rfid = rfidDB.createRfidUser("33333333", groupId3, musicOption3)
+
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
